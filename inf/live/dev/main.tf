19ddd75de6eeaca4ecdc1d3de130cef7715e2c2e
TorrentialFire <wilsontfwi@gmail.com>
2020-11-07 20:49:56 -0600

    👷‍♂️ Revert previous change, free plans don't support webjobs

--- inf/live/dev/main.tf
+++ inf/live/dev/main.tf
@@ -26,11 +26,11 @@ resource "azurerm_app_service" "app" {
   name                = "app-discord-dev"
   resource_group_name = data.terraform_remote_state.shared_resources.outputs.az_shared_rg_name
   location            = data.terraform_remote_state.shared_resources.outputs.az_location
-  app_service_plan_id = data.terraform_remote_state.shared_resources.outputs.az_plan_free_id
+  app_service_plan_id = data.terraform_remote_state.shared_resources.outputs.az_plan_basic_id
   
-  # site_config {
-  #   always_on = true
-  # }
+  site_config {
+    always_on = true
+  }
 
   app_settings = {
     "DISCORD_BOT_TOKEN"     = var.discord_bot_token
